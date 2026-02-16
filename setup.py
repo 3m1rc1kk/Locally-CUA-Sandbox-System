4203c02c74cfea52f11e7992b0202fdf2190d756
Emir Yüksek <147321205+3m1rc1kk@users.noreply.github.com>
2026-02-16 10:36:37 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="cua-system-running-locally-via-sandbox",
+    name="cuaOS",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
